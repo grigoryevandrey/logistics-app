310a796714e34da2d81653cb2f13928654b49e03
Andrey Grigoryev <a.grigoryev@everodelab.com>
2022-03-27 08:41:52 +0300

    chore: monorepo set up

--- README.md
+++ README.md
@@ -4,4 +4,13 @@
 3. Prefix with gazelle (optional?) `gazelle -go_prefix github.com/grigoryevandrey/logistics-app`
 4. Run bazel `bazel run //:gazelle`
 
+## Updating files 
+`gazelle update-repos --from_file=go.mod -to_macro=go_third_party.bzl%go_deps`
+
+## Build
+`bazel build //services/initial`
+
+## Run 
+`bazel run //services/initial`
+
 [Troubleshooting](https://www.tweag.io/blog/2021-09-08-rules_go-gazelle/)
--- services/initial/main.go
+++ services/initial/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 
 	"github.com/grigoryevandrey/logistics-app/services/initial/server"
 )
 
 func main() {
-	log.Println("Setting proxy server port", server.Server())
+	message := server.Server()
+
+	log.Println(message)
+
+    http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprint(w, message)
+    })
+
+	if err := http.ListenAndServe("0.0.0.0:8080", nil); err != nil {
+        log.Fatal(err)
+    }
 }
