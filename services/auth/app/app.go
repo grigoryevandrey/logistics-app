3157f7fc5dad6bec856d017414ff17a7e65630e8
Andrey Grigoryev <adg97@yandex.ru>
2022-05-08 16:19:41 +0300

    feat(auth): implemented login and logout

--- services/auth/app/app.go
+++ services/auth/app/app.go
@@ -1,16 +1,16 @@
 package app
 
 type Service interface {
-	Login(creds LoginCredentials) (*Tokens, error)
-	Logout(tokens Tokens) error
+	Login(creds LoginCredentials, strategy string) (*Tokens, error)
+	Logout(token string, strategy string) error
 }
 
 type LoginCredentials struct {
 	Login    string `json:"login" validate:"min=3,max=255,regexp=^[a-zA-Z0-9]*$"`
-	Password string `json:"password" validate:"min=1,nonnil"`
+	Password string `json:"password" validate:"min=3,max=255,regexp=^[a-zA-Z0-9]*$"`
 }
 
 type Tokens struct {
-	AccessToken  string
-	RefreshToken string
+	AccessToken  string `json:"accessToken"`
+	RefreshToken string `json:"refreshToken"`
 }
--- services/auth/app/transport/transport.go
+++ services/auth/app/transport/transport.go
@@ -1,12 +1,18 @@
 package transport
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/grigoryevandrey/logistics-app/lib/errors"
 	jsonmw "github.com/grigoryevandrey/logistics-app/lib/middlewares/json"
 	"github.com/grigoryevandrey/logistics-app/services/auth/app"
+	"gopkg.in/validator.v2"
 )
+
+const ADMIN_STRATEGY = "admin"
+const MANAGER_STRATEGY = "manager"
 
 type handler struct {
 	app.Service
@@ -45,6 +51,103 @@ func (handlerRef *handler) health(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, gin.H{"status": "UP"})
 }
 
-func (handlerRef *handler) login(ctx *gin.Context) {}
+func (handlerRef *handler) login(ctx *gin.Context) {
+	var credentials app.LoginCredentials
 
-func (handlerRef *handler) logout(ctx *gin.Context) {}
+	query := ctx.Request.URL.Query()
+	strategy := query.Get("strategy")
+
+	if strategy != ADMIN_STRATEGY && strategy != MANAGER_STRATEGY {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "bad strategy"})
+		return
+	}
+
+	err := ctx.ShouldBindJSON(&credentials)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = validator.Validate(credentials)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var tokens *app.Tokens
+
+	switch strategy {
+	case ADMIN_STRATEGY:
+		tokens, err = handlerRef.Login(credentials, strategy)
+	case MANAGER_STRATEGY:
+		tokens, err = handlerRef.Login(credentials, strategy)
+	default:
+		log.Fatalln("Unknown strategy")
+	}
+
+	if err != nil {
+		if err == errors.Error401 {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "bad credentials"})
+			return
+		}
+
+		if err == errors.Error404 {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": "user with this login can not be found"})
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, tokens)
+}
+
+func (handlerRef *handler) logout(ctx *gin.Context) {
+	var tokens app.Tokens
+
+	query := ctx.Request.URL.Query()
+	strategy := query.Get("strategy")
+
+	if strategy != ADMIN_STRATEGY && strategy != MANAGER_STRATEGY {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "bad strategy"})
+		return
+	}
+
+	err := ctx.ShouldBindJSON(&tokens)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = validator.Validate(tokens)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	switch strategy {
+	case ADMIN_STRATEGY:
+		err = handlerRef.Logout(tokens.RefreshToken, strategy)
+	case MANAGER_STRATEGY:
+		err = handlerRef.Logout(tokens.RefreshToken, strategy)
+	default:
+		log.Fatalln("Unknown strategy")
+	}
+
+	if err != nil {
+		if err == errors.Error404 {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": "can not find this active refresh token"})
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.Writer.WriteHeader(204)
+}
